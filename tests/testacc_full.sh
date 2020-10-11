087d1eda495abefe766477b515de17ef938a1e16
awsiv <bishwa.shrestha@gmail.com>
2020-10-11 13:41:47 +0200

    sync with aws and postgresql providers

---
+++ tests/testacc_full.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -e
+
+log() {
+  echo "####################"
+  echo "## ->  $1 "
+  echo "####################"
+}
+
+setup() {
+    "$(pwd)"/tests/testacc_setup.sh
+}
+
+run() {
+  go test -count=1 ./postgresql -v -timeout 120m
+  
+  # keep the return value for the scripts to fail and clean properly
+  return $?
+}
+
+cleanup() {
+    "$(pwd)"/tests/testacc_cleanup.sh
+}
+
+run_suite() {
+    suite=${1?}
+    log "setup ($1)" && setup
+    source "./tests/switch_$suite.sh"
+    log "run ($1)" && run || (log "cleanup" && cleanup && exit 1)
+    log "cleanup ($1)" && cleanup
+}
+
+run_suite "superuser"
+run_suite "rds"
